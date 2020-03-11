c64575baba8a37a3723a47ad03ef9617ef220739
Camck264 <cameronalexandermckenzie@gmail.com>
2020-03-11 21:01:56 +0000

    sudoku 11.03.20

---
+++ sudoku/sudoku.py
@@ -0,0 +1,29 @@
+import turtle
+from random import randit, shuffle
+from time import sleep
+
+
+grid = []
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+myPen = turtle.Turtle()
+myPen.tracer(0)
+myPen.speed(0)
+myPen.color("#00000")
+myPen.hideturtle()
+topLeft_x=-150
+topLeft_y=150
+
+def text(message,x,y,size):
+    FONT = ('Arial', size, 'normal')
+    myPen.penup()
+    myPen.goto(x,y)
+    myPen.write
